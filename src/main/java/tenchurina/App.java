fd3c7542a21f978e8f645eb7179cc58567ff4406
Yulia_Tenchurina <teenager111@yandex.ru>
2021-08-27 13:31:28 +0300

    Создали новый файл и записали в него для каждого из студентов по одному вопросу

--- src/main/java/tenchurina/App.java
+++ src/main/java/tenchurina/App.java
@@ -1,32 +1,31 @@
 package tenchurina;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class App {
-    public static void main( String[] args ) {
-        File file1 = new File("questions.txt", "UTF_8");
-        File file2 = new File("students.txt", "UTF_8");
+    public static void main( String[] args )  {
+
+        File file1 = new File("src\\main\\resources\\questions");
+        File file2 = new File("src\\main\\resources\\students");
+
         ArrayList<String> listQustions = createList(file1);
         ArrayList<String> listStudents = createList(file2);
-        // Перемешиваем вопросы
+         //Перемешиваем вопросы
         listQustions = ShuffleQuestions(listQustions);
+        //Создаем новый файл
+        createFileFull(listQustions,listStudents);
     }
 
-
-
-        public static ArrayList<String> createList(File fileL) {
-            BufferedReader reader;
+        public static ArrayList<String> createList(File file)  {
             ArrayList<String> list = new ArrayList<>();
-            try {
-                reader = new BufferedReader(new FileReader(fileL));
-                String line = reader.readLine();
-                while (line != null) {
-                    list.add(line);
-                    line = reader.readLine();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"))) {
+
+                String sCurrentLine;
+
+                while ((sCurrentLine = br.readLine()) != null) {
+                    list.add(sCurrentLine);
+                   // System.out.println(sCurrentLine);
                 }
             } catch (IOException e) {
                 System.err.println("При чтении файла произошла ошибка " + e);
@@ -40,11 +39,32 @@ public class App {
             for (int i = 0; i < list.size(); i++) {
                 index = (int)(Math.random() * list.size());
                 temp = list.get(i);
-                list.add(i,list.get(index));
-                list.add(index,temp);
-                System.out.println(list.get(i));
+                list.set(i,list.get(index));
+                list.set(index,temp);
             }
+           // list.stream().forEach(System.out::println);
             return list;
+        }
+
+        public static void createFileFull(ArrayList <String> listQ,ArrayList <String> listS){
+            String str = null;
+            char ch= '\n';
+            FileOutputStream fileOut = null;
+            try {
+                fileOut = new FileOutputStream("src\\main\\resources\\general", true);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+                for (int i = 0; i < listS.size(); i++) {
+                    str = listS.get(i) + " " + listQ.get(i);
+                    try {
+                        fileOut.write(str.getBytes());
+                        fileOut.write(ch);
+                    } catch (IOException e) {
+                        System.err.println("Запись в файл не удалась");
+                    }
+                }
         }
 }
 
